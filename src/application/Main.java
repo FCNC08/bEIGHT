5fbef9a3e4bbaef29f7492ffd5a25c11f7f5b3dd
FaCa1109 <capellerfabian@gmail.com>
2023-03-29 21:17:34 +0200

    Commenting Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,27 +17,33 @@ import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
+	
+	//ArrayList with the different Scenes and Runnables to change Scenes with run for example Maximization
 	ArrayList<Scene> Scenes = new ArrayList<>();
 	ArrayList<Runnable> Runnables = new ArrayList<>();
+	
+	//MainStage from start methode
+	
 	Stage MainStage; 
 	
 	@Override
 	public void start(Stage primaryStage) {
+		//Adding different Scenes
 		addStartScene();
 		addLogismArea(); 
-		try {
-			MainStage = primaryStage;
-			changeScene(1);
-			MainStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
+		//set Scene and saves Stage
+		MainStage = primaryStage;
+		changeScene(1);
+		MainStage.show();
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
+	//Menu to change Themes
 	private Menu getThemeChoiceMenu() {
 		Menu theme = new Menu("Themes");
 		MenuItem dark = new MenuItem("Dark");
@@ -49,55 +55,69 @@ public class Main extends Application {
 	}
 	
 	private void addStartScene() {
+		//Adding MenuBar and Adding ThemeChooser
 		MenuBar bar = new MenuBar();
 		bar.getMenus().add(getThemeChoiceMenu());
+		
+		//VBox to add with MenuBar
 		VBox vbox = new VBox(bar);
 		Scene scene = new Scene(vbox);
-		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		//Adding Runnable to release maximization 
 		Runnables.add(new Runnable() {
 			@Override
 			public void run() {
 				MainStage.setMaximized(false);
 			}});
+		
+		//Adding Scene
 		Scenes.add(scene);
 	}
 	
+	//Adding LogicScene
 	private void addLogismArea() {
+		//Adding MenuBar
 		MenuBar bar = new MenuBar();
 		bar.getMenus().add(getThemeChoiceMenu());
 		VBox vbox = new VBox(bar);
+		
+		//Adding SubScene
 		Group root = new Group();
 		SubScene MainScene = new SubScene(root,1000,500);
 		MainScene.heightProperty().bind(vbox.heightProperty());
 		MainScene.widthProperty().bind(vbox.widthProperty());
 		MainScene.setFill(Color.GRAY);
 		
-		
+		//Adding LogicScene
 		LogicSubScene logicscene = LogicSubScene.init(LogicSubScene.cross_distance*73, LogicSubScene.cross_distance*40, 2); 
 		logicscene.setFill(Color.AQUA);
 		
 		logicscene.addX(50);
 		logicscene.addY(25);
 		
-		//logicscene.addWire(LogicSubScene.cross_distance*40, LogicSubScene.cross_distance*21,LogicSubScene.cross_distance*39, LogicSubScene.cross_distance*20);
-		
-		//logicscene.addZTranslate(100);
-		
 		root.getChildren().add(logicscene);
 		vbox.getChildren().add(MainScene);
+		
 		Scene scene = new Scene(vbox);
 		
+		//Adding Runnable to maximize and resize
 		Runnables.add(new Runnable() {
 			public void run() {
 				MainStage.setMaximized(true);
 				MainStage.setResizable(true);
 			}
 		});
+		
+		//Adding Scene
 		Scenes.add(scene);
 	}
 	
+	//Changing Scene method
 	private void changeScene(int SceneNumber) {
+		//Setting Scene to Stage
 		MainStage.setScene(Scenes.get(SceneNumber));
+		
+		//Runs Runnable
 		Runnables.get(SceneNumber).run();
 	}
 }
