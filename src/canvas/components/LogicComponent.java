068cc7c0a8dd820a9f122a642825cae00cbf8bf2
FCNC08 <129294492+FCNC08@users.noreply.github.com>
2023-05-04 22:00:54 +0200

    images

---
+++ src/canvas/components/LogicComponent.java
@@ -0,0 +1,11 @@
+package canvas.components;
+
+public class LogicComponent extends FunctionalCanvasComponent{
+
+	public LogicComponent(int width, int height, int input_count, int output_count, int[] inputs_x, int[] inputs_y,
+			int[] outputs_x, int[] outputs_y) {
+		super(width, height, input_count, output_count, inputs_x, inputs_y, outputs_x, outputs_y);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/canvas/components/StandardComponents/LogicComponents/ANDGate.java
@@ -0,0 +1,13 @@
+package canvas.components.StandardComponents.LogicComponents;
+
+import canvas.components.LogicComponent;
+
+public class ANDGate extends LogicComponent{
+
+	public ANDGate(int width, int height, int input_count, int output_count, int[] inputs_x, int[] inputs_y,
+			int[] outputs_x, int[] outputs_y) {
+		super(width, height, input_count, output_count, inputs_x, inputs_y, outputs_x, outputs_y);
+		// TODO Auto-generated constructor stub
+	}
+
+}
