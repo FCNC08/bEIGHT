63056a64fdfeae420c2ca0f620b9ef2e8600100a
Capeller.Fabian <Capeller.Fabian@wir.local>
2023-05-17 09:23:23 +0200

    Components Chooser

--- src/canvas/components/LogicComponent.java
+++ src/canvas/components/LogicComponent.java
@@ -4,6 +4,9 @@ import javafx.scene.image.Image;
 
 public abstract class LogicComponent extends FunctionalCanvasComponent{
 
+	protected static double StandardWidth = 200;
+	protected static double StandardHeight = 200;
+	
 	public LogicComponent(int width, int height, int input_count, int output_count, int[] inputs_x, int[] inputs_y, int[] outputs_x, int[] outputs_y) {
 		super(width, height, input_count, output_count, inputs_x, inputs_y, outputs_x, outputs_y);
 		// TODO Auto-generated constructor stub
