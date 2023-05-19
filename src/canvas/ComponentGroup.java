be3adf2bdd8bdd8a84d9beb4cf35bc086d9774ea
Capeller.Fabian <Capeller.Fabian@wir.local>
2023-05-19 10:36:10 +0200

    Added Files ComponentGroupingSystem

---
+++ src/canvas/ComponentGroup.java
@@ -0,0 +1,13 @@
+package canvas;
+
+import java.util.ArrayList;
+
+import canvas.components.FunctionalCanvasComponent;
+
+public class ComponentGroup extends ArrayList<FunctionalCanvasComponent>{
+	private static final long serialVersionUID = -3759945142768807045L;
+
+	public ComponentGroup() {
+	}
+	
+}
