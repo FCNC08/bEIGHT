f31c21c5091b68b3a482a342ac05b7c06b4959e7
FCNC <capellerfabian@gmail.com>
2024-07-06 15:31:31 +0200

    Added EducationSbuSceneContainer.java

---
+++ src/education/EducationSubSceneContainer.java
@@ -0,0 +1,65 @@
+package education;
+
+import java.util.ArrayList;
+
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.SubScene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+public class EducationSubSceneContainer extends SubScene{
+	private double width;
+	private double height;
+	
+	private Group root;
+	private FlowPane fpane;
+	private ArrayList<SubScene> sections = new ArrayList<SubScene>();
+	
+	
+	public EducationSubSceneContainer(double width, double height) {
+		super(new Group(), width, height);
+		this.root = (Group) getRoot();
+		this.width = width;
+		this.height = height;
+		
+		Group main_scene_root = new Group();
+		SubScene main_scene = new SubScene(main_scene_root, width, height);
+		
+		fpane = new FlowPane();
+		fpane.setPrefWrapLength(width*0.9);
+		
+		main_scene_root.getChildren().addAll(fpane);
+		sections.add(main_scene);
+		root.getChildren().add(main_scene);
+	}
+	
+	public void addLesson(ZipFile file) {
+		try {
+			EducationSubScene scene = new EducationSubScene(width, height, file, this);
+			
+			final int number = sections.size();
+			sections.add(scene);
+			EventHandler<MouseEvent> open_event_handler = new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					root.getChildren().clear();
+					root.getChildren().add(sections.get(number));
+				}
+			};
+			scene.getIcon().addEventFilter(MouseEvent.MOUSE_CLICKED, open_event_handler);
+			//editor.getIcon().setLayoutX(hbox.getBoundsInLocal().getWidth());;
+			fpane.getChildren().add(scene.getIcon());
+		} catch (ZipException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void gotoStart() {
+		root.getChildren().clear();
+		root.getChildren().add(sections.get(0));
+	}
+
+}
