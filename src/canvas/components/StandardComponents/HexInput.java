22a241f8116c7897f5659c9a5dc0f4d767021093
FCNC08 <129294492+FCNC08@users.noreply.github.com>
2025-07-08 16:26:23 +0200

    Created HexInput

---
+++ src/canvas/components/StandardComponents/HexInput.java
@@ -0,0 +1,53 @@
+package canvas.components.StandardComponents;
+
+import canvas.components.FunctionalCanvasComponent;
+
+public class HexInput extends FunctionalCanvasComponent{
+
+	public HexInput(int width, int height, String size) throws IllegalArgumentException {
+		super(width, height, 0, 4, size);
+	}
+
+	@Override
+	public void simulater() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public FunctionalCanvasComponent getClone(String size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void createInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setVerilogString(short[] comp_count) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setArduinoString(short[] comp_count) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createLayerGate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void resetStandardImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
