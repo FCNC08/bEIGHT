db77f8829ba0da91d49033d816b1957b50d799e5
FCNC <capellerfabian@gmail.com>
2025-07-30 14:59:28 +0200

    Worked on HexInput

--- src/canvas/components/StandardComponents/HexInput.java
+++ src/canvas/components/StandardComponents/HexInput.java
@@ -1,11 +1,29 @@
 package canvas.components.StandardComponents;
 
+
 import canvas.components.FunctionalCanvasComponent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 public class HexInput extends FunctionalCanvasComponent{
 
+	protected static EventHandler<MouseEvent> click_handler = new EventHandler<MouseEvent>() {
+		@Override
+		public void handle(MouseEvent me) {
+			
+		}
+	};
+	
 	public HexInput(int width, int height, String size) throws IllegalArgumentException {
 		super(width, height, 0, 4, size);
+		
+		paintImage();
+		image_view.addEventFilter(MouseEvent.MOUSE_CLICKED, click_handler);
+		
+	}
+	
+	public void paintImage() {
+		
 	}
 
 	@Override
